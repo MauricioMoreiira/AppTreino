49a1f00a917ec557617f8af7ee509e48f5c2dfae
Diego Gafforelli Perfeito <dperfeito@outlook.com>
2022-01-07 20:42:17 -0300

    Alterações Diego

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace AppTreino
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
