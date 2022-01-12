cb6698256636998605aa902a122abf4e67ec1b8b
Mauricio Moreira <mauricionetos182@gmail.com>
2022-01-12 16:11:10 -0300

    [pages] changes layout

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -5,7 +5,6 @@ namespace AppTreino.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
