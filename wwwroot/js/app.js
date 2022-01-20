e87eb2bd454dabd07a05906e21e0b9ce8e268bda
Mauricio Moreira <mauricionetos182@gmail.com>
2022-01-20 16:14:52 -0300

    [ALL MODALS] UPDATE

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -1,10 +1,18 @@
 ﻿function OpenMenu(action) {
 
     if (action.toLowerCase() === "Index") {
-        RedirectAction("/Index");
+        window.location = "./Index";
     }
 
-    if (action.toLowerCase() === "Login") {
-        RedirectAction("/Login");
+    if (action.toLowerCase() === "ConsultaAtleta") {
+        window.location = "./ConsultaAtleta";
+    }
+
+    if (action.toLowerCase() === "AddTopico") {
+        window.location = "./AddTopico";
+    }
+
+    if (action.toLowerCase() === "ConsultaTreinoAtleta") {
+        window.location = "./ConsultaTreinoAtleta";
     }
 }
