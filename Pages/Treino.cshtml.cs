d280a10853b2f9e8b3f56e0b4821bafd4bd9f2de
Mauricio Moreira <mauricionetos182@gmail.com>
2022-02-01 18:34:32 -0300

    add obj no cshtml

--- Pages/Treino.cshtml.cs
+++ Pages/Treino.cshtml.cs
@@ -2,15 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AppTreinoCarlos.Models;
+using AppTreinoCarlos.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace AppTreinoCarlos.Pages
 {
-    public class LoginModel : PageModel
+    public class TreinoModel : PageModel
     {
-        public void OnGet()
+        private static CommFunctions _model;
+        public TreinoModel(IConfiguration configuration)
         {
+            _model = new CommFunctions(configuration);
+        }
+
+        public void OnGet(string idTreino)
+        {
+            ViewData["AtletaTreino"] = _model.GetAtletasTreino(idTreino);
         }
     }
 }
