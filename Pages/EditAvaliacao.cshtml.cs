fe3371b2c711c4f9c17afc61434ddb89f69d1a38
Mauricio Moreira <mauricionetos182@gmail.com>
2022-02-05 16:52:41 -0300

    changes

--- Pages/EditAvaliacao.cshtml.cs
+++ Pages/EditAvaliacao.cshtml.cs
@@ -2,15 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AppTreinoCarlos.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace AppTreinoCarlos.Pages
 {
     public class EditAvaliacaoModel : PageModel
     {
-        public void OnGet()
+        private static CommFunctions _model;
+        public EditAvaliacaoModel(IConfiguration configuration)
         {
+            _model = new CommFunctions(configuration);
+        }
+        public void OnGet(string idTreino, string idAtleta, string NomeAtleta)
+        {
+            ViewData["Avaliacoes"] = _model.GetAvaliacoesTreino(idTreino, idAtleta);
+            ViewData["Atleta"] = NomeAtleta ?? "";
         }
     }
 }
---
+++ Utils/Share.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppTreinoCarlos.Utils
+{
+    public static class Share
+    {
+        public static Color CorIcon { get; set; }
+        public static Color CorButton { get; set; }
+        public static Color CorPadrao { get; set; }
+
+        static Share()
+        {
+            CorPadrao = Color.Purple;
+            CorIcon = Color.Purple;
+            CorButton = Color.Purple;
+        }
+    }
+}
